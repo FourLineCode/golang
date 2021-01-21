1718e21a1aae83dda61904a8e9e4df1334bcd08e
ItzAkmal <akmal3535.ah@gmail.com>
2021-01-21 23:29:45 +0600

    Load added

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/fourlinecode/golang/filereader"
+import (
+	"fmt"
+	"log"
+
+	"github.com/fourlinecode/golang/filereader"
+)
 
 func main() {
 	page := filereader.Page{Title: "A new world", Body: []byte("This is the body for the book.")}
 
 	page.Save()
+
+	newPage, err := filereader.Load("Hello world")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Title: %s\nBody: %s", newPage.Title, newPage.Body)
 }
