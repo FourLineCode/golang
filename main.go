0727e8dde3442488aefb972c4496f50bcecdea5f
ItzAkmal <akmal3535.ah@gmail.com>
2021-01-21 23:22:08 +0600

    filereader added

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import "github.com/fourlinecode/golang/filereader"
 
 func main() {
-	fmt.Println("Hello world")
+	page := filereader.Page{Title: "A new world", Body: []byte("This is the body for the book.")}
+
+	page.Save()
 }
